15f5049692beb48343c5592f6246e66ea7fea5fd
nahem <nahemseguias@gmail.com>
2023-12-30 00:30:12 +0100

    added FundMe.sol contract but importing is broken in VS code

--- fundme/FundMe.sol
+++ fundme/FundMe.sol
@@ -2,8 +2,22 @@
 
 pragma solidity 0.8.19;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
     function fund() public payable {
         require(msg.value >= 1e17, "Didn't send enough ETH");
     }
+
+    function getPrice() public view returns (uint256) {
+        // Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        // Price of ETH in USD. Needs to adjust decimals from 6 to 18.
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return uint256(price) * 1e10;
+    }
+
+    function getConversionRate() public {}
 }
