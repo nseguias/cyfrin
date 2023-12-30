5bd10a1d3b402d8a9deaa5605f7c6b551a562da5
nahem <nahemseguias@gmail.com>
2023-12-30 23:48:52 +0100

    added withdraw function to FundMe.sol

--- fundme/FundMe.sol
+++ fundme/FundMe.sol
@@ -19,4 +19,24 @@ contract FundMe {
         );
         addressToAmountFunded[msg.sender] += msg.value;
     }
+
+    function withdraw() public {
+        uint256 amountToWithdraw = addressToAmountFunded[msg.sender];
+        addressToAmountFunded[msg.sender] = 0;
+
+        // There're 3 ways to send ETH to an address, the last one is the preferred one
+
+        // transfer
+        // payable(msg.sender).transfer(amountToWithdraw);
+
+        // send
+        // bool suceessfulSend = payable(msg.sender).send(amountToWithdraw);
+        // require(suceessfulSend, "Failed to send funds");
+
+        // call
+        (bool successfulCall, ) = payable(msg.sender).call{
+            value: amountToWithdraw
+        }("");
+        require(successfulCall, "Failed to call with value");
+    }
 }
