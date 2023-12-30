de1c470fa66bbaba956d80728ea93ab05e7c6608
nahem <nahemseguias@gmail.com>
2023-12-30 20:27:08 +0100

    created my own library

--- fundme/FundMe.sol
+++ fundme/FundMe.sol
@@ -2,36 +2,21 @@
 
 pragma solidity 0.8.19;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
+
     uint256 minUsdAmount = 5e18;
+
     mapping(address funder => uint256 amountFunded)
         public addressToAmountFunded;
 
     function fund() public payable {
         require(
-            getConversionRate(msg.value) >= minUsdAmount,
+            msg.value.getConversionRate() >= minUsdAmount,
             "Didn't send enough ETH"
         );
         addressToAmountFunded[msg.sender] += msg.value;
-    }
-
-    function getPrice() public view returns (uint256) {
-        // Price feed address for ETH / USD in Sepolia: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
-        // Price of ETH in USD. Needs to adjust decimals from 6 to 18.
-        (, int256 price, , , ) = priceFeed.latestRoundData();
-        return uint256(price) * 1e10;
-    }
-
-    function getConversionRate(
-        uint256 ethAmount
-    ) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethAmount * ethPrice) / 1e18;
-        return ethAmountInUsd;
     }
 }
